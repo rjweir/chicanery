c6ed02d0ed2df9ce7a5e21744524f4bde34c8952
Rob Weir <rweir@ertius.org>
2009-12-13 09:48:49 +1100

    rename stuff.

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ import sys, os
 
 version = '0.0.1'
 
-setup(name='basicpythonproject',
+setup(name='chicanery',
       version=version,
-      description="Basic Python Project",
-      long_description="""\
-""",
+      description="duplicity wrapper",
+      long_description="""""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      keywords='duplicity backup sysadmin',
       author='Rob Weir',
       author_email='rweir@ertius.org',
-      url='http://ertius.org/code/basicpythonproject/',
+      url='http://ertius.org/code/chicanery/',
       license='GNU GPL v2',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
